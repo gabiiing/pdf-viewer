0dcda111b998fd826dd87e53d7a9c519238c63c4
gabiiing <gabiiing13@gmail.com>
2025-10-31 15:40:19 +0700

    Fixing X-Frame

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+      <iframe
+        src="/viewer.html"
+        width="100%"
+        height="100%"
+        style={{ border: "none", height: "100vh", width: "100vw" }}
+        title="PDF Viewer"
+      ></iframe>
+    );
+  }
+  
